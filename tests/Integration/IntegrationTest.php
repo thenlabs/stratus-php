0f8e6a00938fa34330aaf73f3b4b738681c1e3f5
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-05-10 10:57:58 -0400

    Temp

---
+++ tests/Integration/IntegrationTest.php
@@ -0,0 +1,201 @@
+<?php
+declare(strict_types=1);
+
+namespace ThenLabs\StratusPHP\Tests\Functionals;
+
+use ThenLabs\StratusPHP\Tests\SeleniumTestCase;
+use ThenLabs\StratusPHP\JavaScript\JavaScriptClassInterface;
+use ThenLabs\StratusPHP\JavaScript\JavaScriptInstanceInterface;
+use ThenLabs\StratusPHP\AbstractApp;
+use ThenLabs\ComposedViews\AbstractCompositeView;
+use ThenLabs\ClassBuilder\ClassBuilder;
+
+setTestCaseNamespace(__NAMESPACE__);
+setTestCaseClass(SeleniumTestCase::class);
+
+testCase('IntegrationTest.php', function () {
+    testCase(function () {
+        createMacro('tests', function () {
+            test(function () {
+                static::findElement('button')->click();
+                static::waitForResponse();
+
+                $this->assertNotEmpty(
+                    static::findElement('label')->getText()
+                );
+            });
+        });
+
+        testCase(function () {
+            setUpBeforeClassOnce(function () {
+                $app = new class('') extends AbstractApp {
+                    public function getView(): string
+                    {
+                        return <<<HTML
+                            <!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <title>Document</title>
+                            </head>
+                            <body>
+                                <label></label>
+                                <button>Button</button>
+                            </body>
+                            </html>
+                        HTML;
+                    }
+
+                    public function listener($event): void
+                    {
+                        $app = $event->getApp();
+                        $label = $app->querySelector('label');
+
+                        $label->innerHTML = uniqid();
+                        $label->style = 'color: red';
+                    }
+                };
+
+                $app->querySelector('button')->click([$app, 'listener']);
+
+                static::dumpApp($app);
+                static::openApp();
+            });
+
+            useMacro('tests');
+
+            test(function () {
+                $this->assertEquals(
+                    'red',
+                    $this->executeScript('return document.querySelector("label").style.color')
+                );
+            });
+        });
+
+        testCase(function () {
+            setUpBeforeClassOnce(function () {
+                $app = new class('') extends AbstractApp {
+                    public function getView(): string
+                    {
+                        return <<<HTML
+                            <!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                                <meta charset="UTF-8">
+                                <title>Document</title>
+                            </head>
+                            <body>
+                                <label></label>
+                                <button>Button</button>
+                            </body>
+                            </html>
+                        HTML;
+                    }
+                };
+
+                $app->querySelector('button')->click(function ($event) {
+                    $app = $event->getApp();
+                    $label = $app->querySelector('label');
+
+                    $label->innerHTML = uniqid();
+                });
+
+                static::dumpApp($app);
+                static::openApp();
+            });
+
+            useMacro('tests');
+        });
+    });
+
+    testCase(function () {
+        setUpBeforeClassOnce(function () {
+            $app = new class('') extends AbstractApp {
+                public function getView(): string
+                {
+                    return <<<HTML
+                        <!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <title>Document</title>
+                        </head>
+                        <body>
+                            <label></label>
+                            <button>Button</button>
+                        </body>
+                        </html>
+                    HTML;
+                }
+            };
+
+            $app->querySelector('button')->click(function ($event) {
+                $label = $event->getApp()->querySelector('label');
+
+                $label->innerHTML = uniqid();
+                $label->css('color', 'red');
+            });
+
+            static::dumpApp($app);
+            static::openApp();
+        });
+
+        test(function () {
+            static::findElement('button')->click();
+            static::waitForResponse();
+
+            $this->assertEquals(
+                'red',
+                $this->executeScript('return document.querySelector("label").style.color')
+            );
+        });
+    });
+
+    // testCase(function () {
+    //     setUpBeforeClassOnce(function () {
+    //         $app = new class('') extends AbstractApp {
+    //             public function getView(): string
+    //             {
+    //                 return <<<HTML
+    //                     <!DOCTYPE html>
+    //                     <html lang="en">
+    //                     <head>
+    //                         <meta charset="UTF-8">
+    //                         <title>Document</title>
+    //                     </head>
+    //                     <body>
+    //                         <input type="" name="">
+    //                         <label></label>
+    //                         <button>Button</button>
+    //                     </body>
+    //                     </html>
+    //                 HTML;
+    //             }
+    //         };
+
+    //         $app->querySelector('button')->click(function ($event) {
+    //             $app = $event->getApp();
+    //             $input = $app->querySelector('input');
+    //             $label = $app->querySelector('label');
+
+    //             $label->innerHTML = $input->value;
+    //         });
+
+    //         static::dumpApp($app);
+    //         static::openApp();
+    //     });
+
+    //     test(function () {
+    //         $secret = uniqid();
+    //         $input = static::findElement('input');
+    //         $button = static::findElement('button');
+    //         $label = static::findElement('label');
+
+    //         $input->sendKeys($secret);
+    //         $button->click();
+    //         static::waitForResponse();
+
+    //         $this->assertEquals($secret, $label->getText());
+    //     });
+    // });
+});
