8c295b9b00e9357e8ebf7af52f92cd0ab28bcb73
Andy Daniel Navarro Taño <andaniel05@gmail.com>
2020-11-22 09:19:50 -0500

    Temp

--- src/Event/SleepChildEvent.php
+++ src/Event/SleepChildEvent.php
@@ -6,17 +6,28 @@ namespace ThenLabs\StratusPHP\Event;
 use ThenLabs\Components\ComponentInterface;
 
 /**
+ * An event of this type is triggered before that the instance will be serialized.
+ *
  * @author Andy Daniel Navarro Taño <andaniel05@gmail.com>
  */
 class SleepChildEvent extends Event
 {
+    /**
+     * @var ComponentInterface
+     */
     protected $child;
 
+    /**
+     * @param ComponentInterface $child
+     */
     public function __construct(ComponentInterface $child)
     {
         $this->child = $child;
     }
 
+    /**
+     * @return ComponentInterface
+     */
     public function getChild(): ComponentInterface
     {
         return $this->child;
